7093eae977816764a5ee5eb56ef325bf7176fb0f
Momin1807011 <rahman1807011@stud.kuet.ac.bd>
2022-05-07 22:16:08 +0600

    Assignment file has been added.

---
+++ app/src/androidTest/java/git_assignment/git_assign1.java
@@ -0,0 +1,26 @@
+package git_assignment;
+
+public class git_assign1 {
+    int roll,age;
+
+    public git_assign1(int roll, int age) {
+        this.roll = roll;
+        this.age = age;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
